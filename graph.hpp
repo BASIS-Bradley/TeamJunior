93040d290ac123e1521e05439bfcfeaa43daa8e6
Joshua Tensuan <joshua.tensuan99@gmail.com>
2020-03-25 12:16:05 -0700

    Added Edges functionality

--- graph.hpp
+++ graph.hpp
@@ -13,12 +13,14 @@ class graph {
         bool directed;
         bool weighted;
         int num_nodes;
+        int count_edges;
     public:
         graph() {
             lis = new vector<vector<point>>;
             num_nodes = 0;
             directed = false;
             weighted = false;
+            count_edges = 0;
         }
 
         graph(bool x, bool y) {
@@ -26,6 +28,7 @@ class graph {
             num_nodes = 0;
             directed = x;
             weighted = y;
+            count_edges = 0;
         }
 
         graph(bool x, bool y, int num) {
@@ -36,30 +39,35 @@ class graph {
             for(int i = 0; i < num; i++) {
                 lis -> push_back(vector<point>());
             }
+            count_edges = 0;
         }
 
-        void addPoint(int a, int b) { // only works if number nodes is given
+        void addEdge(int a, int b) { // only works if number nodes is given
             if(weighted) {
                 cout << "graph is weighted but no weight given" << endl;
             } else { // I guess space efficiency can be way more efficient if graph is given as undirected
                 if(directed) {
                     lis -> at(a).push_back(point(b, 1));
+                    ++count_edges;
                 } else {
                     lis -> at(a).push_back(point(b, 1));
                     lis -> at(b).push_back(point(a, 1));
+                    ++count_edges;
                 }
             }
         }
 
-        void addPoint(int a, int b, int w) { // only works if number nodes is given
+        void addEdge(int a, int b, int w) { // only works if number nodes is given
             if(!weighted) {
                 cout << "graph is unweighted but weight is given" << endl;
             } else {
                 if(directed) {
                     lis -> at(a).push_back(point(b, w));
+                    ++count_edges;
                 } else {
                     lis -> at(a).push_back(point(b, w));
                     lis -> at(b).push_back(point(a, w));
+                    ++count_edges;
                 }
             }
         }
@@ -76,6 +84,12 @@ class graph {
                 }
             }
         }
+
+        int countEdges() {
+            return count_edges;
+        }
+
+        
 };
 
 #endif
