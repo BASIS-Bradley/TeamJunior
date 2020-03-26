d2b532c1af5c0e17f32d10356c73c7ff6ccc2fc6
toast <toastts@icloud.com>
2020-03-26 04:15:47 -0700

    added file reading methods for each type of graph, definitely room for improvement with the implementation for our graphs though this stuff with the bools doesn't make much sense

--- main.cpp
+++ main.cpp
@@ -4,6 +4,53 @@
 
 using namespace std;
 
+graph graph_ud_uw(false,false);
+graph graph_ud_w(false,true);
+graph graph_d_uw(true,false);
+graph graph_d_w(false,true);
+
+
+
+void read_ud_uw (graph g&){
+    ifstream infile("./data/facebook_undirected.txt");
+    int a, b;
+    while (infile >> a >> b) {
+        g.addpoint(a,b);
+    }
+    
+}
+
+
+void read_d_uw(graph g&){
+    ifstream infile(""); //change file path
+    int a, b;
+    while (infile >> a >> b) {
+        g.addpoint(a,b);
+    }
+    
+}
+
+void read_ud_w(graph g&){
+    ifstream infile(""); //change file path
+    int a, b, c;
+    while (infile >> a >> b >> c) {
+        g.addpoint(a, b, c);
+    }
+    
+}
+
+
+void read_d_w(graph g&){
+    ifstream infile(""); //change file path
+    int a, b, c;
+    while (infile >> a >> b >> c) {
+        g.addpoint(a, b, c);
+    }
+    
+}
+
+
+
 int main() {
     
 }
