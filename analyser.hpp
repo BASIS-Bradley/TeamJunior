3fed6ee42d849af5c980ae1ffdaabf10c4c8a292
toast <toastts@icloud.com>
2020-03-26 16:51:49 -0700

    i wrote this without any intellisense or anything but i think i fixed it

--- analyser.hpp
+++ analyser.hpp
@@ -2,6 +2,7 @@
 #define ANALYSER_HPP_
 
 #include "graph.hpp"
+#include <list>
 
 using namespace std;
 
@@ -37,31 +38,42 @@ class analyser {
             return findOutDegree(a, x);
         }
 
-        // void bfs(node s) {
-        //     node u;
-        //     int i, j;
-        //     queue<node> que;
-        //     bool *visited = new bool[num_nodes];
-        //     for(i = 0; i < num_nodes; i++) {
-        //         visited[i] = false; //not visited
-        //     }
-        //     visited[s.getNum()] = true;//visited
-        //     que.push(s); //insert starting node
-        //     while(!que.empty()){
-        //         u = que.front(); //delete from queue and print
-        //         que.pop();
-        //         cout << char(u.getNum()) << " ";
-        //         for(i = 0; i < num_nodes; i++){
-        //             if(lis -> at(i).at(u.getNum()).getWeight() == 1){
-        //             //when the node is non-visited
-        //                 if(visited[i] == false){
-        //                     visited[i] == true;
-        //                     que.push(visited[i]);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+
+
+        void bfs(node s) {
+
+            //make helper array that keeps tracks of which nodes have been visited, sets it all to false before starting
+            bool *visited = new bool[num_nodes];
+            for(int i = 0; i < num_nodes; i++)
+                visited[i] = false;
+            //creates a queue for the BFS, list data stucture makes sense https://embeddedartistry.com/blog/2017/09/11/choosing-the-right-container-sequential-containers/
+            list<node> queue;
+            //add node that was passed to list of visited and then queue it up
+            visited[s.getNum()] = true;
+            queue.push_back(s);
+
+            while(!queue.empty()) {
+
+                //pop vertex from the front and print it
+                s = queue.front();
+                cout << s << ", ";
+                queue.pop_front();
+
+                // Get all adjacent vertices of the dequeued-
+                // // vertex s. If a adjacent has not been visited,--
+                // // then mark it visited and enqueue it
+                for (auto i = lis -> at(s).begin(); i != lis -> at(s).end(); i++) {
+
+                    if(!visited[*i]) {
+                        visited[*i] = true;
+                        queue.push_back(*i);
+                    }
+                }
+            }
+        }
+
+
+
 };
 
 
