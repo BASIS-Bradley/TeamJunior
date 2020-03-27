7d387d8d326383a958d5288344c2bab3fa608135
Joshua Tensuan <joshua.tensuan99@gmail.com>
2020-03-27 13:40:43 -0700

    Fixed some syntax errors in bfs, still needs testing

--- analyser.hpp
+++ analyser.hpp
@@ -40,30 +40,31 @@ class analyser {
 
 
 
-        void bfs(node s) {
-
+        void bfs(graph a, node s) {
+            int num_nodes = a.get_number_nodes();
             //make helper array that keeps tracks of which nodes have been visited, sets it all to false before starting
             bool *visited = new bool[num_nodes];
-            for(int i = 0; i < num_nodes; i++)
+            for(int i = 0; i < num_nodes; i++) {
                 visited[i] = false;
+            }
             //creates a queue for the BFS, list data stucture makes sense https://embeddedartistry.com/blog/2017/09/11/choosing-the-right-container-sequential-containers/
             list<node> queue;
             //add node that was passed to list of visited and then queue it up
             visited[s.getNum()] = true;
             queue.push_back(s);
+            vector<vector<point>>* lis = a.get_lis();
 
             while(!queue.empty()) {
 
                 //pop vertex from the front and print it
                 s = queue.front();
-                cout << s << ", ";
+                cout << s.getNum() << ", ";
                 queue.pop_front();
 
                 // Get all adjacent vertices of the dequeued-
                 // // vertex s. If a adjacent has not been visited,--
                 // // then mark it visited and enqueue it
                 for (auto i = lis -> at(s).begin(); i != lis -> at(s).end(); i++) {
-
                     if(!visited[*i]) {
                         visited[*i] = true;
                         queue.push_back(*i);
