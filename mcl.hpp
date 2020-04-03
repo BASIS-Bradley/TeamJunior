8352ecaa88c062686eb71ce81c01711e8e92ead2
Joshua Tensuan <joshua.tensuan99@gmail.com>
2020-04-03 15:58:21 -0700

    Forgot to save files on last push

--- mcl.hpp
+++ mcl.hpp
@@ -14,15 +14,26 @@ class mcl {
 
         }
         
-        Eigen::SparseMatrix<float,1,double> mcl_unweighted(graph g, double expand_factor = 2, double inflate_factor = 2, double max_loop = 10) {
+        Eigen::SparseMatrix<double> mcl_unweighted(graph g, double expand_factor = 2, double inflate_factor = 2, double max_loop = 10) {
             if(g.get_weighted) {
                 std::cout << "Given a weighted graph" << std::endl;
                 return;
             }
             std::vector<std::vector<point>>* lis = g.get_lis2();
-            Eigen::SparseMatrix<float,1,double> trans_mat;
-            trans_mat.resize((g.get_number_nodes(), g.get_number_nodes()));
+            Eigen::SparseMatrix<double> trans_mat;
+            trans_mat.resize(g.get_number_nodes(), g.get_number_nodes());
             trans_mat.reserve(g.get_edges());
+            int row = 0;
+            for(auto i = lis -> begin(); i != lis -> end(); i++) {
+                int sum = 0;
+                for(auto j = i -> begin(); j != i -> end(); j++) {
+                    sum += j -> getWeight();
+                }
+                for(auto j = i -> begin(); j != i -> end(); j++) {
+                    trans_mat.insert(row, (j -> getNode()).getNum()) = j -> getWeight();
+                }
+                ++row;
+            }
 
         }
 };
